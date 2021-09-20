766ff192eb88806ca8a53028bccc0e55559dac32
duyhiep2519 <uyhiep2519@gmail.com>
2021-09-20 15:26:03 +0700

    update utils/types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -8,6 +8,25 @@ export enum Role {
    PM = 'pm',
    Member = 'member',
 }
+export enum TaskStatus {
+   Requesting = 1,
+   Unscheduled = 2,
+   Doing = 3,
+   Reviewing = 4,
+   Completed = 5,
+   Cancelled = 6,
+}
+export enum ProjectStatus {
+   Requesting = 1,
+   Pending = 2,
+   InProgress = 3,
+   Completed = 4,
+   Cancelled = 5,
+}
+export enum Priority {
+   High = 'high',
+   Medium = 'medium',
+}
 export type User = {
    id: number;
    username: string;
@@ -16,31 +35,36 @@ export type User = {
    avatar?: any;
    firstName: string;
    lastName: string;
-   roles: Role;
+   role: Role;
    active: boolean;
    dateOfBirth: string;
 };
 export type Project = {
    id: number;
    name: string;
-   status: string;
+   status: ProjectStatus;
    client: string;
-   description: string;
-   startDate: string;
-   endDate: string;
+   description?: string;
+   startDate?: string;
+   endDate?: string;
    member: User[];
    pm: User;
 };
 export type Task = {
+   id: number;
    title: string;
-   notes: string;
+   notes?: string;
    assign: User;
    dueDate: string;
-   status: string;
+   status: TaskStatus;
+   priority: Priority;
 };
 export type Report = {
+   id: number;
    title: string;
    user: User;
    date: string;
    notes: string;
+   note?: string;
+   link: string;
 };
