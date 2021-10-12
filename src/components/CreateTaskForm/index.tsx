bbac72377b5a901ef67fec398849b68bd4180670
duyhiep2519 <uyhiep2519@gmail.com>
2021-10-12 09:05:44 +0700

    fix UX

--- src/components/CreateTaskForm/index.tsx
+++ src/components/CreateTaskForm/index.tsx
@@ -73,7 +73,7 @@ function CreateTaskForm({ onClose, projectId }: CreateTaskFormProps) {
                         value={values.title || ''}
                         onChange={handleChange}
                      />
-                     <div className="createTaskForm__err">{errors.title}</div>
+                     <span className="createTaskForm__err">{errors.title}</span>
                   </div>
                </div>
                <div className="createTaskForm__item">
@@ -81,52 +81,57 @@ function CreateTaskForm({ onClose, projectId }: CreateTaskFormProps) {
                   <div className="createTaskForm__wrap">
                      <textarea
                         rows={4}
-                        cols={25}
+                        cols={31}
                         name="notes"
                         value={values.notes || ''}
                         onChange={handleChange}
                      />
-                     <div className="createTaskForm__err">{errors.notes}</div>
+                     <span className="createTaskForm__err">{errors.notes}</span>
                   </div>
                </div>
                {currentUser?.role === Role.PM && (
                   <div className="createTaskForm__item">
                      <label>Assignee</label>
-                     <select
-                        onChange={(e) => {
-                           if (e.target.value) {
-                              setFormData({
-                                 ...formData,
-                                 assign: USERS.find(
-                                    (user) => user.id === Number(e.target.value)
-                                 ),
-                              });
-                           }
-                        }}
-                     >
-                        {currentProject?.members?.map((member) => (
-                           <option
-                              value={member.id}
-                           >{`${member.lastName} ${member.email}`}</option>
-                        ))}
-                     </select>
+                     <div className="createTaskForm__wrap">
+                        <select
+                           onChange={(e) => {
+                              if (e.target.value) {
+                                 setFormData({
+                                    ...formData,
+                                    assign: USERS.find(
+                                       (user) =>
+                                          user.id === Number(e.target.value)
+                                    ),
+                                 });
+                              }
+                           }}
+                        >
+                           {currentProject?.members?.map((member) => (
+                              <option
+                                 value={member.id}
+                              >{`${member.lastName} ${member.email}`}</option>
+                           ))}
+                        </select>
+                     </div>
                   </div>
                )}
                {currentUser?.role === Role.Member && (
                   <div className="createTaskForm__item">
                      <label> Assignee</label>{' '}
-                     {`${currentProject.pm?.firstName} ${currentProject.pm?.lastName}-${currentProject.pm?.email}`}
+                     <div className="createTaskForm__wrap">{`${currentProject.pm?.firstName} ${currentProject.pm?.lastName}-${currentProject.pm?.email}`}</div>
                   </div>
                )}
                <div className="createTaskForm__item">
                   <label>Due date</label>
-                  <input
-                     type="date"
-                     name="dueDate"
-                     onChange={(e) =>
-                        setFormData({ ...formData, dueDate: e.target.value })
-                     }
-                  />
+                  <div className="createTaskForm__wrap">
+                     <input
+                        type="date"
+                        name="dueDate"
+                        onChange={(e) =>
+                           setFormData({ ...formData, dueDate: e.target.value })
+                        }
+                     />
+                  </div>
                </div>
                <div className="createTaskForm__btn">
                   <button disabled={formData.title === undefined} type="submit">
